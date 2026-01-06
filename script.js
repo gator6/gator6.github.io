7bf8c2bcb58a4e9e751351c9d4fd5232c9a9a6de
gator6 <technophobic.turtle@gmail.com>
2026-01-06 01:20:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ const speed = 100;
 let i = 0;
 
 function typeWriter() {
+  const textElement = document.getElementById('text');
+  const cursorElement = document.querySelector('.cursor');
+
   if (i < text.length) {
-    document.getElementById('text').innerHTML += text.charAt(i);
+    cursorElement.classList.remove('blink');
+    
+    textElement.innerHTML += text.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
+  } else {
+    cursorElement.classList.add('blink');
   }
 }
 
